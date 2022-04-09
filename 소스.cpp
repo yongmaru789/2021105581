65967950460b62bf7baeb0aff5a384a1fdb702fc
Jaeung Choi <jaeung0129@gmail.com>
2022-04-09 15:58:14 +0900

    9단 추가

--- 소스.cpp
+++ 소스.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-	for (int i = 1; i <= 8; i++) {
-		for (int j = 1; j <= 8; j++) {
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
 			cout << i << " * " << j << " = " << i * j << "\t";
 		}
 		cout << endl;
